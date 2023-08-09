6b3dea09c690d129dd40da7f3e3ffa147353d592
박민혁 <129154514+plumas90@users.noreply.github.com>
2023-08-09 14:51:58 +0900

    3초보여주기완성
    3초보여주기완성

--- Assets/Scripts/count321.cs
+++ Assets/Scripts/count321.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
@@ -8,10 +9,13 @@ public class count321 : MonoBehaviour
 {
     public float time = 3;
     public Text count3;
-    
+    public AudioSource audioSource;
+    public AudioClip start;
+
     // Start is called before the first frame update
     void Start()
     {
+        startSound();
         time = 3f;
     }
 
@@ -19,10 +23,22 @@ public class count321 : MonoBehaviour
     void Update()
     {
         time -= Time.deltaTime;
-        count3.text = time.ToString("N1");
-        if (time == 0) 
+        count3.text = time.ToString("N0");
+        if (time <= 0.01f) 
         {
             Destroy(gameObject);
+
         }
     }
+    void oneshot() 
+    {
+        audioSource.PlayOneShot(start);
+    }
+    void startSound() 
+    {
+        Invoke("oneshot", 0f);
+        Invoke("oneshot", 1f);
+        Invoke("oneshot", 2f);
+    }
+    
 }
